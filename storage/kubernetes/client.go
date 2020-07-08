59c6ec8f2aa9fc7a08114fb64e01ce4e7fa4cbf2
Benjamin Somhegyi <somhegyi.b@gmail.com>
2020-07-08 11:29:28 +0200

    Increase K8S storage client timeout settings (#14)

--- storage/kubernetes/client.go
+++ storage/kubernetes/client.go
@@ -298,7 +298,7 @@ func newClient(cluster k8sapi.Cluster, user k8sapi.AuthInfo, namespace string, l
 			KeepAlive: 30 * time.Second,
 		}).Dial,
 		TLSClientConfig:       tlsConfig,
-		TLSHandshakeTimeout:   10 * time.Second,
+		TLSHandshakeTimeout:   20 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 
@@ -335,7 +335,7 @@ func newClient(cluster k8sapi.Cluster, user k8sapi.AuthInfo, namespace string, l
 	return &client{
 		client: &http.Client{
 			Transport: t,
-			Timeout:   15 * time.Second,
+			Timeout:   35 * time.Second,
 		},
 		baseURL:    cluster.Server,
 		hash:       func() hash.Hash { return fnv.New64() },
